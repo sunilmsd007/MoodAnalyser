79ac678b0f46fc6a59d9ed297e66d4e852d408a8
sunilmsd007 <sunilmsd007@gmail.com>
2022-10-02 19:09:15 +0530

    Modified: using custom exception informing user invalid mood

--- AnalyseMood.java
+++ AnalyseMood.java
@@ -7,7 +7,7 @@ public class AnalyseMood {
           }
 
 	//method to analyse mood by reading string
-	public String moodAnalyser() {
+	public String moodAnalyser() throws MoodAnalysisException {
                 try{
 		if (message.contains("Happy")) {
 			return "Happy";
@@ -19,13 +19,17 @@ public class AnalyseMood {
                         return "Happy";
 		}
                 }catch(Exception e){
-                 //when user provides null input, handling the exception by returning Happy
-                       return "Happy";
+                 //when user provides null input, informing the user by returning Invalid mood
+                       throw new MoodAnalysisException("Invalid Mood");
                 }
                        return message;
 	}
 	public static void main(String[] args) {
 		AnalyseMood obj = new AnalyseMood(null);
-                System.out.println(obj.moodAnalyser());
+                try{
+                    System.out.println(obj.moodAnalyser());
+                   }catch (Exception p) {
+			p.printStackTrace();
+                    }
        }
 }
---
+++ MoodAnalysisException.java
@@ -0,0 +1,6 @@
+public class MoodAnalysisException extends Exception {
+
+	MoodAnalysisException(String message){
+		super(message);
+	}
+}
